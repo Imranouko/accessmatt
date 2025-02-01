79ebfd9165f1064d4d1288db0ad46e5607776f30
Imranouko <emmanuelochieng49@gmail.com>
2025-02-02 02:03:22 +0300

    Add files via upload

---
+++ text.js
@@ -0,0 +1,47 @@
+function terms(){
+    window.location.href = "Terms.html"
+    }
+function about(){
+   window.location.href = "About.html"
+  }
+function contact(){
+    window.location.href = "Contact.html"
+}
+function policy(){
+   window.location.href = "Policy.html"
+}
+function sitemap(){
+  window.location.href = "Sitemap.html"
+}
+function categories(){
+window.location.href = "categories.html"
+}
+function account(){
+  if (localStorage.getItem("pasword")=="" & localStorage.getItem("pasword")==null){
+    window.location.href = "Account.html"}
+    else{window.location.href = "This.html"}
+}
+function returns(){
+    window.location.href = "Return.html"
+}
+function Service(){
+    window.location.href = "Service.html"
+}
+function cart(){
+    window.location.href = "Cart.html"
+}
+function inst(){
+    window.location.href = "https://www.instagram.com/accessmat?igsh=YzljYTk1ODg3Zg=="
+}
+function yt(){
+    window.location.href = "https://youtube.com/@accessmat?si=dq8TFPxha1yb86zf"
+}
+function fb(){
+    window.location.href = "https://www.facebook.com/profile.php?id=61566608144644"
+}
+function wa(){
+    window.location.href = "https://wa.me/254758400124"
+}
+function shop(){
+    window.location.href = "Shop.html"
+}
